a40eb87c44f29948b2390e6bbb2a9b156b5bde09
Dragos Sandu <sdragosflorian@gmail.com>
2023-06-14 01:32:54 +0300

    Train after adding relu to brain network

--- networks_files/brain.py
+++ networks_files/brain.py
@@ -13,7 +13,9 @@ class Brain(nn.Module):
         super(Brain, self).__init__()
 
         self.fc1 = nn.Linear(2048 * 3, 2048)
+        self.relu1 = nn.ReLU()
         self.fc2 = nn.Linear(2048 + 512, 128)
+        self.relu2 = nn.ReLU()
         self.fc3 = nn.Linear(128, num_classes)
         self.softmax = nn.Softmax(dim=1)
 
@@ -32,7 +34,9 @@ class Brain2(nn.Module):
 
         self.conv1 = nn.Conv2d(1, 1, kernel_size=(3, 1))
         self.fc1 = nn.Linear(2048 * 3, 2048)
+        self.relu1 = nn.ReLU()
         self.fc2 = nn.Linear(2048 + 512, 128)
+        self.relu2 = nn.ReLU()
         self.fc3 = nn.Linear(128, num_classes)
         self.softmax = nn.Softmax(dim=1)
 
@@ -40,7 +44,9 @@ class Brain2(nn.Module):
         x = self.conv1(x)
         x = torch.flatten(x, start_dim=1)
         x = self.fc1(x)
+        x = self.relu1(x)
         x = self.fc2(torch.cat((x, y), dim=1))
+        x = self.relu2(x)
         x = self.fc3(x)
         x = self.softmax(x)
 
